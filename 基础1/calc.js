949cab4da50efde412167d4c06ab743b77631bcf
lc <lc@qq.com>
2017-09-30 23:28:39 +0800

    JS基础案例

---
+++ 基础1/calc.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+function calcSum() {
+    var sum = 0;
+    for (var i = 0; i <= 100; i++) {
+        sum += i;
+    }
+    console.log(sum);
+}
