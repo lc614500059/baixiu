86612b565b9afb0e70d43c599c770a9170ad55ce
lc614500059 <17644200523@163.com>
2017-10-31 22:00:06 +0800

    ajax01

---
+++ AJAX01/login.php
@@ -0,0 +1,14 @@
+<?php
+
+if (empty($_POST['username']) || empty($_POST['password'])) {
+    exit('请提交用户名和密码');
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+if ($username === 'admin' && $password === '123') {
+    exit('成功');
+}
+
+exit('用户名或者密码错误');
+
---
+++ AJAX01/xml.php
@@ -0,0 +1,10 @@
+<?php
+
+header('Content-Type: application/xml')
+?>
+<?xml version="1.1" encoding="utf-8"?>
+<person>
+    <name>梅西</name>
+    <age>29</age>
+    <gender>男</gender>
+</person>
